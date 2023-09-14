0b47a23256dc2d8d58436f8650d86f786432f99c
Roman 111 <romanvydysh7@gmail.com>
2023-09-14 16:57:55 +0300

    Two commit

--- АП_КН-104Б_Видиш_ЛР-02.1/АП_КН-104Б_Видиш_ЛР-02.1/АП_КН-104Б_Видиш_ЛР-02.1.cpp
+++ АП_КН-104Б_Видиш_ЛР-02.1/АП_КН-104Б_Видиш_ЛР-02.1/АП_КН-104Б_Видиш_ЛР-02.1.cpp
@@ -1,8 +1,20 @@
 ﻿
 #include <iostream>
+#include <cmath>
+
+using namespace std;;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double x, y, z1, z2;
+    cout << "x = "; cin >> x;
+    cout << "y = "; cin >> y;
+
+    z1 = pow(cos(x), 4) + pow(sin(y), 2) + 1 / 4 * pow(sin(2 * x - 1), 2);
+    z2 = sin(y + x) * sin(y - x);
+
+    cout << "z1 = " << z1;
+    cout << "z2 = " << z2;
+    
 }
 
