898de65217c37f06901a0c1da25616bc9c9feeb5
Roman 111 <romanvydysh7@gmail.com>
2023-09-21 19:42:08 +0300

    3.2 lab

---
+++ АП_КН-104Б_Видиш_ЛР-03.2/PR1/PR1/PR1.cpp
@@ -0,0 +1,52 @@
+﻿// Lab_03_2.cpp
+// < Видиш Роман >
+// Лабораторна робота № 3.2
+// Розгалуження, задане формулою: функція з параметрами.
+// Варіант 22
+
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double x;
+	double a;
+	double b;
+	double c;
+	double F;
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "x = "; cin >> x;
+	// 1 спосіб
+	if (x + 5 < 0 && c == 0) {
+		F = 1 / ((a * x) - b);
+	};
+	if (x + 5 > 0 && c != 0) {
+		F = (x - a) / x;
+	};
+	if (!(x + 5 < 0 && c == 0) && !(x + 5 > 0 && c != 0)) {
+		F = 10 * x / (c - 4);
+	}
+	cout << endl;
+	cout << "1) F = " << F << endl;
+
+	// 2 спосіб
+	if (x + 5 < 0 && c == 0) {
+		F = 1 / ((a * x) - b);
+	}
+	else if (x + 5 > 0 && c != 0) {
+		F = (x - a) / x;
+	}
+	else {
+		F = 10 * x / (c - 4);
+	}
+	cout << endl;
+	cout << "2) F = " << F << endl;
+
+
+
+}
+
