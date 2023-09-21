7d968d678848e587b30aed0f6fb049a99385846b
Roman 111 <romanvydysh7@gmail.com>
2023-09-21 21:08:22 +0300

    3.3 labs

---
+++ АП_КН-104Б_Видиш_ЛР-03.3/PR1/PR1/PR1.cpp
@@ -0,0 +1,40 @@
+﻿// Lab_03_3.cpp
+// < Видиш Роман >
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 22
+#include <iostream>
+#include <cmath>
+#include <math.h>
+using namespace std;
+int main()
+{
+	double x;
+	double R;
+	double y;
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	// розгалуження в повній формі
+	if (x <= -8) {
+		y = R * (-1);
+	}
+	else if (x > -8 && x <= R * (-1)) {
+		y = ((R * (x + 8)) / (R * (-1)) + 8) - R;
+	}
+	else if (x > R * (-1) && x <= R) {
+			;
+	}
+	else if (x > R && x <= 5) {
+		y = (2 * x - 2 * R) / (5 - R);
+	}
+	else {
+		y = 3;
+	}
+
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
+
