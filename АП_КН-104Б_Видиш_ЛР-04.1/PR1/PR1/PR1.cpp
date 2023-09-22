175d937dd3563232539ad87c87b6f9d20a6c0652
Roman 111 <romanvydysh7@gmail.com>
2023-09-22 18:37:23 +0300

    lab 4.1

---
+++ АП_КН-104Б_Видиш_ЛР-04.1/PR1/PR1/PR1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int k, N, i;
+	double S;
+	cout << "N = "; cin >> N;
+	S = 0;
+	k = 20;
+	i = N;
+	while (i <= k)
+	{
+		S += (cos(i) + sin(i)) / (1 + cos(i) * sin(i));
+		i++;
+	}
+	cout << S << endl;
+	
+	S = 0;
+	i = N;
+	do {
+		S += (cos(i) + sin(i)) / (1 + cos(i) * sin(i));
+		i++;
+	} while (i <= k);
+	cout << S << endl;
+	S = 0;
+	for (i = N; i <= k; i++)
+	{
+		S += (cos(i) + sin(i)) / (1 + cos(i) * sin(i));
+	}
+	cout << S << endl;
+	S = 0;
+	for (i = k; i >= N; i--)
+	{
+		S += (cos(i) + sin(i)) / (1 + cos(i) * sin(i));
+	}
+	cout << S << endl;
+	return 0;
+}
