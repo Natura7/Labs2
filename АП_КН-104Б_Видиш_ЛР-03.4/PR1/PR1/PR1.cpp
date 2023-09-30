6b39f2cf91b5b35e443d9b3b0fac04322ca60a20
Roman 111 <romanvydysh7@gmail.com>
2023-09-30 11:12:08 +0300

    3.4 obn

--- АП_КН-104Б_Видиш_ЛР-03.4/PR1/PR1/PR1.cpp
+++ АП_КН-104Б_Видиш_ЛР-03.4/PR1/PR1/PR1.cpp
@@ -13,9 +13,8 @@ int main()
 	cout << "x = "; cin >> x;
 	cout << "y = "; cin >> y;
 	cout << "R = "; cin >> R;
-
 	// розгалуження в повній формі
-	if (x + y <= R*R && x <= 0 && y >= 0 || x>=0 && y <=0 && y <= 2*x-2*R && y <=  (-2 * R) / R && y <= R * (-1))
+	if (x*x + y*y <= R*R && x <= 0 && y >= 0 || x>=0 && y <=0 && y <=  (-2 * R) / R && y >= -R && y >= 2 * x - 2 * R )
 		cout << "yes" << endl;
 	else
 		cout << "no" << endl;
